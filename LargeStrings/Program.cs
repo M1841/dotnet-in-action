d9f2abdf2c21591af48b6dcf84dcf4aef4d4c4c4
Mihai Mureșan <mihaimuresan592003@gmail.com>
2024-08-20 12:51:53 +0300

    3.3. Verbatim and Raw Strings

---
+++ LargeStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿var bookTitle = ".NET in Action";
+Console.WriteLine(@$"<?xml version=""1.0""?>
+<books>
+  <book title=""{bookTitle}""/>
+</books>
+");
+
+var userId = 12;
+var resourceId = 6;
+Console.WriteLine(
+@$"{{
+  ""RequestParams"": {{
+    ""UserId"": ""{userId}""
+    ""ResourceId"": ""{resourceId}""
+  }}
+}}");
+
+var rawJsonString = """
+{
+  "RequestParams": {
+    "UserId": "someId",
+    "ResourceId": "someOtherId"
+  }
+}
+""";
+Console.WriteLine(rawJsonString);
